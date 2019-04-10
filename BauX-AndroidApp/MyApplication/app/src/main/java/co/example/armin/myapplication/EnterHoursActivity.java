71cfead1f932c54d6c9680a10059af8b17e517ec
Armin Hamzic <arminhamzic2001@hotmail.com>
2019-04-10 22:02:48 +0200

    upload of the app and the systemplan

---
+++ BauX-AndroidApp/MyApplication/app/src/main/java/co/example/armin/myapplication/EnterHoursActivity.java
@@ -0,0 +1,12 @@
+package co.example.armin.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnterHoursActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_hours);
+    }
+}
